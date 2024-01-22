dc48ccf6e402b658c23730d29e4b0bfa505db761
Na4xo <davidsatel033@gmail.com>
2024-01-22 19:17:02 +0300

    Create lastdz.py

---
+++ lastdz.py
@@ -0,0 +1,28 @@
+# Задние 1
+
+a = int(input('Введи первое чилос: '))
+b = int(input('Введи второе число: '))
+c = int(input('Введи третье число: '))
+print(f'Сумма чисел равна: {a+b+c}\nПроизведение чисел равна: {a*b*c}')
+
+# Задание 2
+
+sela = int(input('Зарплата за месяц₽: '))
+platezh = int(input('Месячный платёж по кредиту₽: '))
+dolg = int(input('Задолжность по коммунальным услугам₽: '))
+print(f'У тебя осталось {sela - platezh - dolg}₽')
+
+# Задание 3
+
+Diagonal_first = int(input('Диогональ площади первой стороны: '))
+Diagonal_second = int(input('Диогональ площади второй стороны: '))
+
+print(f'Площадь ромба равна: {(Diagonal_first - Diagonal_second) / 2}')
+
+# Задание 4
+
+print(f'To be\nor not\nto be')
+
+# Задание 5
+
+print(f"«Life is what happens  \n     when \n         you're busy making other plans» \n    
